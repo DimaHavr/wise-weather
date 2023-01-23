9bc7b6da31784e21fa8dff307ff026a8437110fc
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-01-23 21:41:08 +0000

    Deploying to gh-pages from @ DimaHavr/wise-weather@a12af71037479e0016a03ea4ab298c26f88559b9 🚀

---
+++ static/js/544.330fd6c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwise_weather=self.webpackChunkwise_weather||[]).push([[544],{9544:function(e,n,s){s.r(n);var r=s(2791),t=s(1743),u=s(4476),a=s(184),i=(0,r.lazy)((function(){return Promise.all([s.e(419),s.e(69)]).then(s.bind(s,4069))}));n.default=function(){return(0,a.jsx)(u.Z,{children:(0,a.jsx)(r.Suspense,{fallback:(0,a.jsx)(t.Z,{}),children:(0,a.jsx)(i,{})})})}}}]);
+//# sourceMappingURL=544.330fd6c6.chunk.js.map
